991ba15dc8babe20a19c9994705d7f08e39b03da
Anisse <AnisseBen@users.noreply.github.com>
2025-01-26 20:44:00 -0500

    modificationquelques retouches sur la page de l'evenement 2 et son css + CarteEvenement et son css

---
+++ app/evenements/evenement2/page.jsx
@@ -0,0 +1,30 @@
+
+import styles from "./page.module.css"
+import Evenement from "@/components/Evenement";
+import BannerImage from "@/components/BannerImage"
+
+import carte2 from "@/public/carte2.jpg"
+import books from "@/public/books.jpg"
+import meubles from "@/public/meubles.jpg"
+import appareillePhoto from "@/public/appareillePhoto.jpg"
+
+export default function Evenement2() {
+
+    return (
+        <>
+            <BannerImage titre={"Vintage & Antiquités"} />
+            <div className={styles.sectionEvenement}>
+                <Evenement
+                    image0={carte2}
+                    image1={meubles}
+                    image2={appareillePhoto}
+                    image3={books}
+                    themeEvenement={"Vintage & Antiquités"}
+                    contactEvenement={"Contact : Mr Anisse."}
+                    descriptionEvenement={"Vente de garage dédiée aux amateurs de vintage. Collection de disques vinyles, objets rétro, meubles anciens et accessoires uniques."}
+                    adresseEvenement={"Adresse : 1720, Blvd Napoleon, Nepean, Ottawa"}
+                    datEvenement={"Date : 20-Fevrier-2025"} />
+            </div>
+        </>
+    )
+}
--- components/CarteEvenement.jsx
+++ components/CarteEvenement.jsx
@@ -9,6 +9,7 @@ export default function CarteEvenement({theme, contact, adresse,description, dat
             <div className={styles.description}>{description}</div>
             <div className={styles.adresse}>{adresse}</div>
             <div className={styles.dateEvenement}>{dateEvenement}</div>
+            <div className={styles.plusPhotos}><a href="#">Plus de photos &rarr;</a></div>
         </div>
     )
 }
