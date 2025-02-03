64b8e8791f5b013981c0f48bd687f26ffe12bbb3
Ousameur Elias <ousameurelias@gmail.com>
2025-02-03 00:19:43 -0500

    rendre le header responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,37 @@
+"use client";
+
 import styles from "./Header.module.css";
 import stylesBtn from "./Boutton.module.css";
 
 import Image from "next/image";
 import logo from "@/public/Logo-white.png";
-import logoBlack from "@/public/Logo-black.png";
+import btnMenu from "@/public/menuButton.png";
 
 import Boutton from "@/components/Boutton";
 import Link from "next/link";
 
+import { useState } from "react";
+
 export default function Header() {
+  const [estVisible, setVisible] = useState(false);
+
+  const navBarActive = () => {
+    setVisible(!estVisible)
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Image src={logo} alt="Logo React" className={styles.Image} />
       </div>
-      <nav>
+      <div onClick={navBarActive}>
+        <Image
+          src={btnMenu}
+          alt="logo pour le menu pour petit ecran"
+          className={styles.btnMenu}
+        />
+      </div>
+      <nav className={`${styles.nav} ${estVisible && styles.navActive}`}>
         <ul className={styles.liste}>
           <li>
             <Link href="/">Accueil</Link>
@@ -29,11 +46,11 @@ export default function Header() {
             <Link href="#">FAQ</Link>
           </li>
         </ul>
+        <div className={styles.bouttons}>
+          <Boutton texte="S'inscrire" />
+          <Boutton texte="Se connecter" active={true} />
+        </div>
       </nav>
-      <div className={styles.bouttons}>
-        <Boutton texte="S'inscrire" />
-        <Boutton texte="Se connecter" active={true} />
-      </div>
     </header>
   );
 }
