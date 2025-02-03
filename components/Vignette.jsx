4bbb5b9304cceee99977122ee1de3e4970cea5af
Ousameur Elias <ousameurelias@gmail.com>
2025-02-03 11:18:34 -0500

    rendre le footer responsive

--- components/Vignette.jsx
+++ components/Vignette.jsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import styles from "./Vignette.module.css"
+import styles from "./Vignette.module.css";
 
 export default function Vignette({ source, altDescription, titre }) {
-    return (
-        <div className={styles.vignette}>
-            <Image src={source} alt={altDescription} className={styles.image}/>
-            {/* <div className={styles.titreVignette}>{titre}</div> */}
-        </div>
-    );
+  return (
+    <div className={styles.vignette}>
+      <Image src={source} alt={altDescription} className={styles.image} />
+    </div>
+  );
 }
