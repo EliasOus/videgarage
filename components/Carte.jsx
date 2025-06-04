49b5b16a0948118e057d593b776b3ac036435393
Ousameur Elias <ousameurelias@gmail.com>
2025-06-04 12:07:16 -0400

    Modifie le CSS pour activer le mode sombre sur la page contactez-nous"

--- components/Carte.jsx
+++ components/Carte.jsx
@@ -14,7 +14,7 @@ export default function Carte({ image, titre, description, link }) {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
               fill="#ffffff"
-              fill-opacity="1"
+              fillOpacity="1"
               d="M0,256L1440,96L1440,320L0,320Z"
             ></path>
           </svg>
